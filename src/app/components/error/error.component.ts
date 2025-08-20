84a029b839e40a44bc6a0c3ecba77d86ee1a726d
Jade Hendricks <hendricksjade@gmail.com>
2025-08-20 17:05:45 +0200

    add: adding in a loader and error component - to reflect state in the UI

---
+++ src/app/components/error/error.component.ts
@@ -0,0 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error',
+  imports: [],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.scss'
+})
+export class ErrorComponent {
+  @Input() error!: HttpErrorResponse;
+}
